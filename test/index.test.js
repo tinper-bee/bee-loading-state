5223bde9005f8797704d08ddc90ec14d79a51e17
ahua52 <1468492018@qq.com>
2016-10-12 10:00:59 +0800

    新增测试用例

--- test/index.test.js
+++ test/index.test.js
@@ -1,3 +1,11 @@
 import React from 'react';
 import {shallow, mount, render} from 'enzyme';
 import {expect} from 'chai';
+import { Loadingstate } from '../src/index';
+
+describe('Enzyme Shallow', function() {
+	it('Loadingstate should be exist', function() {
+		let Loadingstate = shallow(<Loadingstate/>);
+		expect(Loadingstate.length).to.equal(1);
+	})
+})
