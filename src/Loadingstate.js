4db91a587913d5258d3a61bb61923b0ce6a57f50
ahua52 <1468492018@qq.com>
2016-10-18 14:24:00 +0800

    完善加修改名字

--- src/Loadingstate.js
+++ src/Loadingstate.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes}from 'react';
 import ReactDOM from 'react-dom';
 import classnames from 'classnames';
-import { Button } from '../node_modules/bee-button'
+import { Button } from 'bee-button'
 
 const propTypes = {
 	/**
@@ -22,13 +22,13 @@ const defaultProps = {
 
 const clsPrefix = 'u-button';
 
-class Loadingstate extends React.Component{
+class Loadingstate extends Component{
 	constructor(props){
 		super(props);
 		this.state = {
 			clickFlag: false,
-			loadingText:this.props.children
 		}
+		this.handleClick = this.handleClick.bind(this);
 	}
 	handleClick (){
 		this.setState({clickFlag: true,loadingText:this.props.loadingText});
@@ -36,12 +36,13 @@ class Loadingstate extends React.Component{
 			this.setState({clickFlag: false,loadingText: this.props.children});
 		}.bind(this),this.props.loadingTime)
 	}
+	// 组件移除
 	render(){
 		let {loadingTime,loadingText, ...others} = this.props;
         return (
         	<Button
         		 disabled={this.state.clickFlag}
-        		 onClick={this.handleClick.bind(this)}
+        		 onClick={this.handleClick}
         		 {...others}>
         		{this.state.loadingText}
         	</Button>
